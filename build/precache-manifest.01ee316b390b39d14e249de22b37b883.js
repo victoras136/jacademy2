aeac873c8f3bba1825932fafec622373ef8855bf
victoras136 <103646488+victoras136@users.noreply.github.com>
2024-09-28 14:26:48 +0300

    Add files via upload

---
+++ build/precache-manifest.01ee316b390b39d14e249de22b37b883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7a766ee2e1827d0e75f5351947d075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67c6e6e4ddd331529039",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "166cecbb4ed6d793dadf",
+    "url": "/static/css/main.b2d23492.chunk.css"
+  },
+  {
+    "revision": "67c6e6e4ddd331529039",
+    "url": "/static/js/2.75502a90.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.75502a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166cecbb4ed6d793dadf",
+    "url": "/static/js/main.671ef1b6.chunk.js"
+  },
+  {
+    "revision": "1523dce5e71dca20d2cd",
+    "url": "/static/js/runtime-main.410dd32f.js"
+  }
+]);
